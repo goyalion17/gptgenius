6a8ae3d6a4920ece9f44b3e5383900dce3c8be8a
Irina Saveleva <goyalion17@gmail.com>
2024-03-20 22:36:28 +0100

    building newTour page

--- app/(dashboard)/tours/new-tour/page.js
+++ app/(dashboard)/tours/new-tour/page.js
@@ -1,18 +1,18 @@
-import Chat from "@/components/Chat";
 import {
   HydrationBoundary,
   QueryClient,
   dehydrate,
 } from "@tanstack/react-query";
+import NewTour from "@/components/NewTour";
 
-const ChatPage = () => {
+const NewTourPage = () => {
   const queryClient = new QueryClient();
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Chat />
+      <NewTour />
     </HydrationBoundary>
   );
 };
 
-export default ChatPage;
+export default NewTourPage;
